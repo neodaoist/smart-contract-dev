b7e61ec1d8869991a9872b154e823b6ee7bc1bc0
neodaoist <tomMobileDev123@me.me>
2022-05-11 11:45:30 -0400

    Practice expectRevert

---
+++ src/Cheater.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Cheater {
+
+    error LieTold();
+    error LieToldWithParams(uint256 _input);
+
+    function tellTruth(uint256 _input) public pure returns (bool) {
+        if (_input != 1337) {
+            revert("SOMETHING_WENT_WRONG");
+        }
+
+        return true;
+    }
+
+    function tellTruthWithError(uint256 _input) public pure returns (bool) {
+        if (_input != 1337) {
+            revert LieTold();
+        }
+
+        return true;
+    }
+
+    function tellTruthWithErrorWithParams(uint256 _input) public pure returns (bool) {
+        if (_input != 1337) {
+            revert LieToldWithParams(_input);
+        }
+
+        return true;
+    }
+
+    function tellTruthWithRequire(uint256 _input) public pure returns (bool) {
+        require(_input == 1337, "Requirement not met");
+
+        return true;
+    }
+}
---
+++ test/Cheater.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import 'forge-std/Test.sol';
+import '../src/Cheater.sol';
+
+contract CheaterTest is Test {
+
+    Cheater cheater;
+
+    function setUp() public {
+      cheater = new Cheater();
+    }
+
+    function testTellTruth_shouldRevertIfNot1337() public {
+        vm.expectRevert("SOMETHING_WENT_WRONG");
+
+        cheater.tellTruth(1);
+    }
+
+    function testTellTruth_shouldBeTrueIf1337() public {
+        assertTrue(cheater.tellTruth(1337));
+    }
+
+    function testTellTruthWithError_shouldThrowIfNot1337() public {
+        vm.expectRevert(Cheater.LieTold.selector);
+
+        cheater.tellTruthWithError(1);
+    }
+
+    function testTellTruthWithError_shouldBeFalseIf1337() public {
+        assertTrue(cheater.tellTruthWithError(1337));
+    }
+
+    function testTellTruthWithErrorWithParams_shouldThrowIfNot1337() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Cheater.LieToldWithParams.selector, 1)
+        );
+
+        cheater.tellTruthWithErrorWithParams(1);
+    }
+
+    function testTellTruthWithErrorWithParams_shouldBeTrueIf1337() public {
+        assertTrue(cheater.tellTruthWithErrorWithParams(1337));
+    }
+
+    // how to assert the specific require message ?
+    function testFailTellTruthWithRequire_shouldThrowIfNot1337() public {
+        cheater.tellTruthWithRequire(1);
+    }
+}
