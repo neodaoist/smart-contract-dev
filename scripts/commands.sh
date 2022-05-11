70a4b471eaf8685634d52e450bac60c6db0b76c2
neodaoist <tomMobileDev123@me.me>
2022-05-10 20:24:58 -0400

    Practice L2 deploys with Optimism and Polygon

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,4 +1,10 @@
 # Basic NFT contract deploying, minting, and checking ownerOf
-forge create NFT --rpc-url=$RINKEBY_RPC_URL --private-key=$PRIVATE_KEY --constructor-args "Lyfe NFT" "LYFE"
-cast send --rpc-url=$RINKEBY_RPC_URL <contractAddress> "mintTo(address)" <eoaAddress> --private-key=$PRIVATE_KEY
-cast call --rpc-url=$RINKEBY_RPC_URL --private-key=$PRIVATE_KEY <contractAddress> "ownerOf(uint256)" 1
+forge create NFT --rpc-url=$RPC_URL --private-key=$PRIVATE_KEY --constructor-args "Lyfe NFT" "LYFE"
+cast send --rpc-url=$RPC_URL --private-key=$PRIVATE_KEY <contractAddress> "mintTo(address)" <eoaAddress>
+cast send --rpc-url=$L2_RPC_URL --private-key=$PRIVATE_KEY <contractAddress> "mintTo(address)" <eoaAddress> --legacy --value 0.08ether
+cast call --rpc-url=$RPC_URL --private-key=$PRIVATE_KEY <contractAddress> "ownerOf(uint256)" 1
+cast call --rpc-url=$RPC_URL --private-key=$PRIVATE_KEY <contractAddress> "tokenURI(uint256)" 1
+cast call --rpc-url=$RPC_URL --private-key=$PRIVATE_KEY <contractAddress> "balanceOf(address)" <eoaAddress>
+
+## Etherscan verification
+
