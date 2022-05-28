5a44c25c1ba51d564ecbdfabbcc0c2ab3fd65f02
neodaoist <tomMobileDev123@me.me>
2022-05-28 16:28:30 -0400

    Add initial ERC1155 batch and burn tests

--- test/ERC1155.t.sol
+++ test/ERC1155.t.sol
@@ -32,6 +32,76 @@ contract ERC1155Test is Test {
         assertEq(to.mintData(), "testing 456");
     }
 
+    function testBatchMintToEOA() public {
+        uint256[] memory ids = new uint256[](5);
+        ids[0] = 1337;
+        ids[1] = 1338;
+        ids[2] = 1339;
+        ids[3] = 1340;
+        ids[4] = 1341;
+
+        uint256[] memory amounts = new uint256[](5);
+        amounts[0] = 100;
+        amounts[1] = 200;
+        amounts[2] = 300;
+        amounts[3] = 400;
+        amounts[4] = 500;
+
+        token.batchMint(address(0xBABE), ids, amounts, "");
+
+        assertEq(token.balanceOf(address(0xBABE), 1337), 100);
+        assertEq(token.balanceOf(address(0xBABE), 1338), 200);
+        assertEq(token.balanceOf(address(0xBABE), 1339), 300);
+        assertEq(token.balanceOf(address(0xBABE), 1340), 400);
+        assertEq(token.balanceOf(address(0xBABE), 1341), 500);
+    }
+
+    function testBatchMintToERC1155Recipient() public {
+        ERC1155Recipient to = new ERC1155Recipient();
+
+        uint256[] memory ids = new uint256[](5);
+        ids[0] = 1337;
+        ids[1] = 1338;
+        ids[2] = 1339;
+        ids[3] = 1340;
+        ids[4] = 1341;
+
+        uint256[] memory amounts = new uint256[](5);
+        amounts[0] = 100;
+        amounts[1] = 200;
+        amounts[2] = 300;
+        amounts[3] = 400;
+        amounts[4] = 500;
+
+        token.batchMint(address(to), ids, amounts, "testing 456");
+
+        assertEq(to.batchOperator(), address(this));
+        assertEq(to.batchFrom(), address(0));
+        // assertUintArrayEq(to.batchIds(), ids);
+        for (uint i = 0; i < ids.length; i++) {
+            assertEq(to.batchIds()[i], ids[i]);
+        }
+        // assertUintArrayEq(to.batchAmounts(), amounts);
+        for (uint j = 0; j < amounts.length; j++) {
+            assertEq(to.batchAmounts()[j], amounts[j]);
+        }
+        assertEq(to.batchData(), "testing 456");
+
+        assertEq(token.balanceOf(address(to), 1337), 100);
+        assertEq(token.balanceOf(address(to), 1338), 200);
+        assertEq(token.balanceOf(address(to), 1339), 300);
+        assertEq(token.balanceOf(address(to), 1340), 400);
+        assertEq(token.balanceOf(address(to), 1341), 500);
+    }
+
+    function testBurn() public {
+        token.mint(address(0xBABE), 1337, 100, "");
+
+        token.burn(address(0xBABE), 1337, 70);
+
+        assertEq(token.balanceOf(address(0xBABE), 1337), 30);
+    }
+
     //
 
 }
@@ -60,6 +130,33 @@ contract ERC1155Recipient is ERC1155TokenReceiver {
         return ERC1155TokenReceiver.onERC1155Received.selector;
     }
 
-    //
+    address public batchOperator;
+    address public batchFrom;
+    uint256[] internal _batchIds;
+    uint256[] internal _batchAmounts;
+    bytes public batchData;
 
+    function batchIds() external view returns (uint256[] memory) {
+        return _batchIds;
+    }
+
+    function batchAmounts() external view returns (uint256[] memory) {
+        return _batchAmounts;
+    }
+
+    function onERC1155BatchReceived(
+        address _operator,
+        address _from,
+        uint256[] calldata _ids,
+        uint256[] calldata _amounts,
+        bytes calldata _data
+    ) external override returns (bytes4) {
+        batchOperator = _operator;
+        batchFrom = _from;
+        _batchIds = _ids;
+        _batchAmounts = _amounts;
+        batchData = _data;
+
+        return ERC1155TokenReceiver.onERC1155BatchReceived.selector;
+    }
 }
