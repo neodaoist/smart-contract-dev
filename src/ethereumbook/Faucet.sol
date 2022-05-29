145b6ea0c7183e4f30c6c0f8f3b9ef99ecc7b8dc
neodaoist <tomMobileDev123@me.me>
2022-05-29 15:10:25 -0400

    Add faucet contract from Mastering Ethereum book

---
+++ src/ethereumbook/Faucet.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.4;
+
+contract Faucet {
+
+    function withdraw(uint withdraw_amount) public {
+        require(withdraw_amount <= 100000000000000000);
+
+        msg.sender.transfer(withdraw_amount);
+    }
+
+    receive() external payable {}
+}
