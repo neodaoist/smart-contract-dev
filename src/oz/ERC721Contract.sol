bf441c6355f54be35da9c4b60d010a0a45d7de9e
neodaoist <tom@loudverse.com>
2022-06-26 14:41:56 -0400

    :sparkles: Start wtesting OZ 721 w/ metadata

--- src/oz/ERC721Contract.sol
+++ src/oz/ERC721Contract.sol
@@ -6,4 +6,8 @@ import {ERC721} from "openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract ERC721Contract is ERC721 {
     //
     constructor() ERC721("Epique NFT", "EPIQUE") {}
+
+    function tokenURI(uint256 tokenID) public view virtual override returns (string memory) {
+        return "ipfs://bafkreifueofjqgxahz2wthmjgj64pkiota4v5oxvvvgfzthx234lljag7i";
+    }
 }
