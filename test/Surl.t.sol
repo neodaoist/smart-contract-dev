c4dc1a8a4d8871dcd6ddad9cf33142c4989bec4b
neodaoist <tom@loudverse.com>
2022-07-25 00:04:50 +0300

    :sparkles: More surl

--- test/Surl.t.sol
+++ test/Surl.t.sol
@@ -1,15 +1,96 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.15;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+
 import {Surl} from "surl/Surl.sol";
+import {strings} from "solidity-stringutils/strings.sol";
 
 contract SurlTest is Test {
-    //
     using Surl for *;
+    using strings for *;
 
-    function run() public {
-        (uint256 status, bytes memory data) = "https://book.getfoundry.sh/".get();
-        emit logs(data);
+    function setUp() public {}
+
+    function testBasicSurl() public {
+        (uint256 status, bytes memory data) = "https://jsonplaceholder.typicode.com/todos/1".get();
+
+        assertEq(status, 200);
+        assertEq(string(data), '{  "userId": 1,  "id": 1,  "title": "delectus aut autem",  "completed": false}');
+    }
+
+    function testGet() public {
+        (uint256 status, bytes memory data) = "https://jsonplaceholder.typicode.com/todos/1".get();
+
+        assertEq(status, 200);
+        assertEq(string(data), '{  "userId": 1,  "id": 1,  "title": "delectus aut autem",  "completed": false}');
+    }
+
+    function testGetOptions() public {
+        string[] memory headers = new string[](2);
+        headers[0] = "accept: application/json";
+        headers[1] = "Authorization: Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==";
+        (uint256 status, bytes memory data) = "https://httpbin.org/headers".get(headers);
+
+        assertEq(status, 200);
+
+        strings.slice memory responseText = string(data).toSlice();
+        assertTrue(responseText.contains(("QWxhZGRpbjpvcGVuIHNlc2FtZQ==").toSlice()));
+        assertTrue(responseText.contains(("application/json").toSlice()));
+    }
+
+    function testPostFormData() public {
+        string[] memory headers = new string[](1);
+        headers[0] = "Content-Type: application/x-www-form-urlencoded";
+        (uint256 status, bytes memory data) = "https://httpbin.org/post".post(
+            headers,
+            "formfield=myemail@ethereum.org"
+        );
+
+        assertEq(status, 200);
+
+        strings.slice memory responseText = string(data).toSlice();
+        assertTrue(responseText.contains(("formfield").toSlice()));
+        assertTrue(responseText.contains(("myemail@ethereum.org").toSlice()));
+    }
+
+    function testPostJson() public {
+        string[] memory headers = new string[](1);
+        headers[0] = "Content-Type: application/json";
+        (uint256 status, bytes memory data) = "https://httpbin.org/post".post(headers, '{"foo": "bar"}');
+
+        assertEq(status, 200);
+        strings.slice memory responseText = string(data).toSlice();
+        assertTrue(responseText.contains(("foo").toSlice()));
+        assertTrue(responseText.contains(("bar").toSlice()));
+    }
+
+    function testPut() public {
+        (uint256 status, ) = "https://httpbin.org/put".put();
+
+        assertEq(status, 200);
+    }
+
+    function testPutJson() public {
+        string[] memory headers = new string[](1);
+        headers[0] = "Content-Type: application/json";
+        (uint256 status, bytes memory data) = "https://httpbin.org/put".put(headers, '{"foo": "bar"}');
+
+        assertEq(status, 200);
+        strings.slice memory responseText = string(data).toSlice();
+        assertTrue(responseText.contains(("foo").toSlice()));
+        assertTrue(responseText.contains(("bar").toSlice()));
+    }
+
+    function testDelete() public {
+        (uint256 status, ) = "https://httpbin.org/delete".del();
+
+        assertEq(status, 200);
+    }
+
+    function testPatch() public {
+        (uint256 status, ) = "https://httpbin.org/patch".patch();
+
+        assertEq(status, 200);
     }
 }
