66e86dd195fe64beddeeeb05adff60951e123202
neodaoist <tom@loudverse.com>
2022-08-15 19:55:55 +0200

    :recycle: CImprove contract layout and formatting

--- src/BasicBaboons.sol
+++ src/BasicBaboons.sol
@@ -1,11 +1,12 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.15;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
 
 import {ERC721} from "openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721Royalty} from "openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 import {Counters} from "openzeppelin/contracts/utils/Counters.sol";
 import {Ownable} from "openzeppelin/contracts/access/Ownable.sol";
 
+/// @dev Container library for events, in order to share between production and test code
 library BasicBaboonEvents {
     event Withdrawal(uint256 amount);
     event MaxSupplyUpdated(uint256 newSupply);
@@ -14,61 +15,93 @@ library BasicBaboonEvents {
     event RoyaltyUpdated(address indexed receiver, uint96 royaltyPercentageInBips);
 }
 
+/// @notice Basic Baboons is a ground-breaking, totally original, totally awesome NFT collection =)
 contract BasicBaboons is ERC721Royalty, Ownable {
     //
     using Counters for Counters.Counter;
 
-    Counters.Counter internal nextId;
+    /*//////////////////////////////////////////////////////////////
+                        Public State Variables
+    //////////////////////////////////////////////////////////////*/
 
     uint256 public maxSupply = 1000;
-    uint256 internal constant MINT_PRICE = 0.05 ether;
+    mapping(address => bool) public allowlisted;
 
     bytes32 public immutable provenanceHash;
+
+    /*//////////////////////////////////////////////////////////////
+                        Internal State Variables
+    //////////////////////////////////////////////////////////////*/
+
+    Counters.Counter internal nextId;
+    string internal baseURI = "https://originaluri.xyz/";
+    bool internal uriFrozen;
 
     uint8 internal immutable teamAllocation;
     address internal immutable teamMultisig;
 
-    mapping(address => bool) public allowlisted;
+    uint256 internal constant MINT_PRICE = 0.05 ether;
+    uint96 internal constant INITIAL_ROYALTY_PERCENTAGE_IN_BIPS = 500;
+    uint96 internal constant MAX_ROYALTY_PERCENTAGE_IN_BIPS = 1000;
 
-    uint96 INITIAL_ROYALTY_PERCENTAGE_IN_BIPS = 500;
-    uint96 MAX_ROYALTY_PERCENTAGE_IN_BIPS = 1000;
-    
-    string internal baseURI = "https://originaluri.xyz/";
+    /*//////////////////////////////////////////////////////////////
+                        Constructor
+    //////////////////////////////////////////////////////////////*/
 
-    bool internal uriFrozen;
-    
     /// @param _teamMultisig Multisig address of the project team
     /// @param _teamAllocation Number of tokens to be minted to the project team
     /// @param _allowlist Addresses to allowlist
     /// @param _provenanceHash 32-byte hash of the metadata
-    constructor (address _teamMultisig, uint8 _teamAllocation, address[] memory _allowlist, bytes32 _provenanceHash) ERC721("Basic Baboons", "BBB") public {
-        nextId.increment(); // start at 1        
+    constructor(
+        address _teamMultisig,
+        uint8 _teamAllocation,
+        address[] memory _allowlist,
+        bytes32 _provenanceHash
+    )
+        public
+        ERC721("Basic Baboons", "BBB")
+    {
+        nextId.increment(); // start at tokenId 1
 
         teamMultisig = _teamMultisig;
         teamAllocation = _teamAllocation;
         provenanceHash = _provenanceHash;
 
-        transferOwnership(teamMultisig);
-        setupAllowlist(_allowlist);
-        mintTeamAllocation();
+        _transferOwnership(teamMultisig);
+        _setupAllowlist(_allowlist);
+        _mintTeamAllocation();
         _setDefaultRoyalty(teamMultisig, INITIAL_ROYALTY_PERCENTAGE_IN_BIPS);
     }
 
-    function setupAllowlist(address[] memory _allowlist) internal {
+    function _setupAllowlist(address[] memory _allowlist) internal {
         for (uint256 i = 0; i < _allowlist.length; i++) {
             allowlisted[_allowlist[i]] = true;
         }
     }
 
-    function mintTeamAllocation() internal {
+    function _mintTeamAllocation() internal {
         for (uint256 i = 0; i < teamAllocation; i++) {
             _mint(teamMultisig);
         }
     }
 
+    /*//////////////////////////////////////////////////////////////
+                        External/Public Views
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Retrieve the total number of tokens minted
+    /// @return Total tokens minted
+    function totalSupply() public view returns (uint256) {
+        return nextId.current() - 1;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        External/Public Transactions
+    //////////////////////////////////////////////////////////////*/
+
     /// @notice Mint an NFT to the sender
     function mint() external payable {
-        require(msg.value == MINT_PRICE, "Mint price of 0.05 ETH not paid");        
+        require(msg.value == MINT_PRICE, "Mint price of 0.05 ETH not paid");
         require(nextId.current() <= maxSupply, "Max supply already reached");
 
         _mint(msg.sender);
@@ -100,35 +133,27 @@ contract BasicBaboons is ERC721Royalty, Ownable {
     /// @notice Reduce the max supply
     /// @param _newSupply New max supply
     function reduceSupply(uint256 _newSupply) external onlyOwner {
-        require(_newSupply < maxSupply && _newSupply >= totalSupply(),
-            "New supply must be < previous max supply and >= total supply");
+        require(
+            _newSupply < maxSupply && _newSupply >= totalSupply(),
+            "New supply must be < previous max supply and >= total supply"
+        );
 
         maxSupply = _newSupply;
 
         emit BasicBaboonEvents.MaxSupplyUpdated(_newSupply);
     }
 
-    /// @notice Retrieve the total number of tokens minted
-    /// @return Total tokens minted
-    function totalSupply() public view returns (uint256) {
-        return nextId.current() - 1;
-    }
-
-    function _baseURI() internal view override returns (string memory) {
-        return baseURI;
-    }
-
-    /// @notice
-    /// @param
-    function setURI(string memory _uri) public onlyOwner {
+    /// @notice Set a new URI
+    /// @param _newURI New URI string
+    function setURI(string memory _newURI) public onlyOwner {
         require(!uriFrozen, "URI is frozen and cannot be updated");
 
-        baseURI = _uri;
+        baseURI = _newURI;
 
-        emit BasicBaboonEvents.URIUpdated(_uri);
+        emit BasicBaboonEvents.URIUpdated(_newURI);
     }
 
-    /// @notice 
+    /// @notice Freeze the URI, preventing any more updates
     function freezeURI() public onlyOwner {
         require(!uriFrozen, "URI already frozen");
 
@@ -141,10 +166,21 @@ contract BasicBaboons is ERC721Royalty, Ownable {
     /// @param _newRoyaltyPercentageInBips The new royalty percentage in basis points,
     /// must not exceed 10% (1000 bips)
     function setNewRoyalty(uint96 _newRoyaltyPercentageInBips) external onlyOwner {
-        require(_newRoyaltyPercentageInBips <= MAX_ROYALTY_PERCENTAGE_IN_BIPS, "New royalty percentage must not exceed 10%");
+        require(
+            _newRoyaltyPercentageInBips <= MAX_ROYALTY_PERCENTAGE_IN_BIPS,
+            "New royalty percentage must not exceed 10%"
+        );
 
         _setDefaultRoyalty(owner(), _newRoyaltyPercentageInBips);
 
         emit BasicBaboonEvents.RoyaltyUpdated(owner(), _newRoyaltyPercentageInBips);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        Internal/Private Views
+    //////////////////////////////////////////////////////////////*/
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
     }
 }
