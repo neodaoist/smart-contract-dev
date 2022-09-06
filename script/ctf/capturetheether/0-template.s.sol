b45d2f08f7d5469c5b88acb611eba37ad27873cb
neodaoist <tom@loudverse.com>
2022-09-06 11:15:16 -0400

    :golf: Add CTF scripting template

---
+++ script/ctf/capturetheether/0-template.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+contract XYZScript is Script {
+    //
+    
+    function run() public {
+        //
+    }
+}
+
+interface XYZChallenge {
+    //
+}
